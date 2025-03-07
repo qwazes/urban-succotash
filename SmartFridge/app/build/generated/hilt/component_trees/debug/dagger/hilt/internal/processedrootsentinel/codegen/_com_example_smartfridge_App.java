5ca55b6a67eeda1e5f5763885f77758048f8b1c5
qwazes <andrewmir459@gmail.com>
2025-03-07 14:16:12 +0300

    Delete SmartFridge directory

--- SmartFridge/app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_example_smartfridge_App.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.example.smartfridge.App"
-)
-public final class _com_example_smartfridge_App {
-}
--- SmartFridge/app/src/main/java/com/example/smartfridge/App.kt
+++
@@ -1,8 +0,0 @@
-package com.example.smartfridge
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class App : Application() {
-}
